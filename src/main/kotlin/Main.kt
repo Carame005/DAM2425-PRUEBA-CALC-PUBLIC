a44bbfc89f84a80fed66e6f6561e55b2461f0c89
Caramelo <cccarlos@iesrafaelalberti.es>
2025-04-11 18:07:47 +0200

    Ficheros comienzo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,15 @@ package es.iesraprog2425.pruebaes
 
 import es.iesraprog2425.pruebaes.app.Calculadora
 import es.iesraprog2425.pruebaes.ui.Consola
+import es.iesraprog2425.pruebaes.ui.IEntradaSalida
 import es.iesraprog2425.pruebaes.utils.IUtilsFicheros
 import java.io.File
 
-fun main(fich : IUtilsFicheros) {
-
-val log = "./log"
-if (fich.existeFichero(log)){
-    val f = File(log)
-}else{
-    val mkdir = Mkdir("./log")
-}
-
+fun main(args : Array<String>) {
+val fich : IUtilsFicheros
+val ui : IEntradaSalida = Consola()
+val log = "src/main/kotlin/log"
+val directorio = File("./log")
     Calculadora(Consola()).iniciar()
 }
 
